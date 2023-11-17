d44c5f9e1bd675b443f1dd45809d7e37dad1f085
balazsbanyasz <31312753+balazsbanyasz@users.noreply.github.com>
2023-11-17 17:05:28 +0100

    Update hoffGetter.sh

--- hoffGetter.sh
+++ hoffGetter.sh
@@ -2,4 +2,4 @@
 rand=$((1 + $RANDOM % 4))
 user=$(whoami)
 curl -O "https://raw.githubusercontent.com/balazsbanyasz/work/main/hoff$rand.png"
-osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"Users/$user/Downloads/hoff2.png\" as POSIX file"
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"Users/$user/Downloads/$rand.png\" as POSIX file"
